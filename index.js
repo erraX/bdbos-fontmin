2efe3c21ffb2db3821441bef8cbda3e6c3146353
niminjie <niminjie@baidu.com>
2016-10-31 14:09:23 +0800

    ISSUE=4991245 new BosFontmin api

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 var utils = require('./lib/utils');
 var FontminRunner = require('./lib/FontminRunner');
-var BosUplaoder = require('./lib/BosUploader');
+var BosUploader = require('./lib/BosUploader');
 var CssProcessor = require('./lib/CssProcessor');
 
-function runner(options) {
-    var fontminRunner = new FontminRunner(options);
-    var bosUplaoder = new BosUplaoder(options);
-    var cssProcessor = new CssProcessor(options);
-
-    var processCss = function (css) {
-        return utils.compose(
-            cssProcessor.tripComments,
-            cssProcessor.addCdnHost
-        ).bind(cssProcessor);
-    };
-
-    return fontminRunner.run()
-        .then(function (files) {
-            return bosUplaoder.run(files);
-        })
-        .then(function (files) {
-            files.css.content = processCss(files.css.content);
-            return files;
-        });
+var PLUGIN_MAP = {
+    fontmin : FontminRunner,
+    bos     : BosUploader,
+    css     : CssProcessor
 };
 
-module.exports = runner;
+function BosFontmin() {
+    this._plugins = [];
+}
+
+BosFontmin.prototype._validatePluginStream = function (plugin) {
+    if (!this._plugins.length && plugin !== 'fontmin') {
+        throw 'fontmin must be register firstly!';
+    }
+};
+
+BosFontmin.prototype.register = function (plugin, config) {
+    if (!plugin || !utils.isString(plugin)) {
+        return;
+    }
+
+    plugin = plugin.toLowerCase();
+    var Plugin = PLUGIN_MAP[plugin];
+
+    if (!Plugin) {
+        return;
+    }
+
+    this._validatePluginStream(plugin);
+
+    this._plugins.push({
+        name: plugin,
+        plugin: new Plugin(config)
+    });
+};
+
+BosFontmin.prototype.run = function () {
+    var fontmin = this._plugins[0].plugin;
+
+    return this._plugins.slice(1).reduce(function (chain, target) {
+        return chain.then(target.plugin.run.bind(target.plugin));
+    }, fontmin.run());
+};
+
+module.exports = BosFontmin;
